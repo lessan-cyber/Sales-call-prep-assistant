8d329d02fc32fc8d0ddd5cfc99f6d172e3f29f9f
lessan-cyber <azizmetchonou434@gmail.com>
2025-10-29 21:08:08 +0000

    test(profile): Add unit tests for user profile page
    Configure Jest, add setup files, update package.json with test script, and create unit tests for the ProfilePage component.

---
+++ frontend/jest.config.js
@@ -0,0 +1,18 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/lib/(.*)$': '<rootDir>/src/lib/$1',
+    '^@/types/(.*)$': '<rootDir>/src/types/$1',
+  },
+  moduleDirectories: ['node_modules', '<rootDir>/node_modules'],
+};
+
+module.exports = createJestConfig(customJestConfig);
